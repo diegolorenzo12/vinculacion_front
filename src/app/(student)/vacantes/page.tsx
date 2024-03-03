76f30aec81fdd95c6829ccc595a70b5798b4965b
Cesar <crenteriaejr@gmail.com>
2024-03-02 21:03:55 -0800

    added page group estudiante

---
+++ src/app/(student)/vacantes/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return(
+        <h1>Vacantes</h1>
+    )
+}
--- src/app/components/HeaderItem.tsx
+++ src/app/components/HeaderItem.tsx
@@ -1,8 +1,12 @@
+'use client'
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function HeaderItem({text, href} : {text:string, href:string}){
+    const isActive = usePathname() === href
     return(
-        <div className="flex self-center text-center font-bold w-32">
+        <div className={`flex self-center place-items-center justify-center text-center font-bold h-min w-32 ${isActive ? 'border-b-2 border-yellow' : ''}`}>
             <Link href={href}>{text}</Link>
         </div>
     )
